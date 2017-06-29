8e356ca6520e7e97129f7161a422b44630f8f1de
John Boronkas <johnboronkas@gmail.com>
2017-06-28 23:39:25 -0400

    Working..

--- src/index.js
+++ src/index.js
@@ -3,32 +3,35 @@
 var APP_ID = 'amzn1.ask.skill.dc5fa280-1660-4aa3-aefb-d2f824ae60eb';
 var Alexa = require('alexa-sdk');
 
-var handlers = {
-    'NewSession': function() {
-        this.emit(':ask', 'Lets get started. I\'ll say a series of cards and you tell me what the card count is using the REKO method. 7, 3, Jack. What\'s the current count?');
+var handlers =  {
+    'LaunchRequest': function() {
+        // var msg = 'Lets get started! I\'ll say a series of cards and you tell me what the card count is using the REKO method. 7, 3, Jack. What\'s the current count?';
+        this.emit(':ask', 'Lets get started! What\'s the count?');
     },
     'AMAZON.StopIntent': function() {
-        this.emit(':tell', 'Goodbye!');  
+        this.emit(':tell', 'Stop Intent. Goodbye!');  
     },
     'AMAZON.CancelIntent': function() {
-        this.emit(':tell', 'Goodbye!');  
+        this.emit(':tell', 'Cancel Intent. Goodbye!');  
     },
     'AMAZON.HelpIntent': function() {
         this.emit(':tell', 'I intend to help, but I don\'t know how to yet.');
     },
     'SessionEndedRequest': function () {
-        this.emit(':tell', 'Goodbye!');
+        this.emit(':tell', 'Session Ended. Goodbye!');
+    },
+    'Unhandled': function() {
+        this.emit(':tell', 'Unhandled. Sorry, I didn\'t get that.', 'What\'s the current count?');
     },
     'AnswerIntent': function () {
         var answer = parseInt(this.event.request.intent.slots.number.value);
+        console.log(answer);
         if (answer == 1) {
             this.emit(':tell', 'You are correct!');
         } else {
-            this.emit(':tell', 'Incorrect, the count is 1.');
+            this.emit(':tell', 'Incorrect, the count is currently 1.');
         }
-
-        // this.emit(':ask', ''); // TODO
-    }
+    },
 };
 
 exports.handler = function(event, context, callback){
